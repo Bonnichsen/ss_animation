8dc99fc3a98dd973e30d2d4c981587dd496d1068
Benjamin Reinhold Bonnichsen <Benjamin@Benjamin-Reinhold-Bonnichsens-MacBook-Pro.local>
2017-11-09 14:50:44 +0100

    scene 1 done

--- java.js
+++ java.js
@@ -1,6 +1,20 @@
-$(document).ready(function () {
+$(window).on("load", startAnim);
 
+function startAnim() {
+    console.log("hånd kommer ind");
+    $("#hand_container").addClass("hand_move_up");
+    $("#lower").addClass("lower_hand");
     setTimeout(function () {
-        $("#phone").fadeIn(1000);
-    }, 1000);
-});
+        $("#phone").fadeIn(500);
+    }, 500);
+    $("#hand_container").on("animationend", fingers);
+}
+
+function fingers() {
+    console.log("fingre kommer ind")
+    $("#hand_container").off("animationend", fingers);
+
+    $("#lower").addClass("lower_hand");
+    $("#hand_container").addClass("fingers");
+    $("#hand_container").removeClass("hand_move_up");
+}
