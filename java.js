96fe9442016c8b518aaa2b9a41caa67e077b6507
amandadittmer <>
2017-11-10 19:52:36 +0100

    new

--- java.js
+++ java.js
@@ -28,37 +28,37 @@ function fingers() {
 function besked() {
     console.log("besked kommer ind")
     $("#hand_container").off("animationend", besked);
-    $("#besked").addClass("besked_ind");
-    $("#stjerne").addClass("stjerne_ind");
+    $("#besked_container").addClass("besked_ind");
+    $("#stjerne_container").addClass("stjerne_ind");
 
     setTimeout(censoreret, 3000);
-    $("#stjerne").on("animationend", censoreret);
+    $("#stjerne_container").on("animationend", censoreret);
 }
 
 
 function censoreret() {
     console.log("censoreret billede");
     $("#hand_container").off("animationend", censoreret);
-    $("#besked").removeClass("besked_ind");
-    $("#stjerne").removeClass("stjerne_ind");
+    $("#besked_container").removeClass("besked_ind");
+    $("#stjerne_container").removeClass("stjerne_ind");
     $("#lower").removeClass("lower_hand");
     $("#phone").hide();
     $("#hand_container").removeClass("fingers");
     $("#hand_container").removeClass("hand_move_up");
 
-    $("#mobil").addClass("mobil_c");
-    $("#censoreret").addClass("censoreret_billede");
+    $("#mobil_container").addClass("mobil_c");
+    $("#censoreret_container").addClass("censoreret_billede");
     setTimeout(vidersend, 2000);
 }
 
 
 function vidersend() {
     console.log("vidersend billede");
-    $("#mobil").addClass("mobil_c");
-    $("#censoreret").removeClass("censoreret_billede");
-    $("#mobil").addClass("mobil_c");
-    $("#knap").addClass("vidersend");
-    $("#finger").addClass("finger_move_up");
+    $("#mobil_container").addClass("mobil_c");
+    $("#censoreret_container").removeClass("censoreret_billede");
+    $("#mobil_container").addClass("mobil_c");
+    $("#knap_container").addClass("vidersend");
+    $("#finger_container").addClass("finger_move_up");
 
 
 }
