ea4f9c71b8311ad0052cb463c305f822c3190b9c
amandadittmer <>
2017-11-13 13:41:02 +0100

    idag

--- java.js
+++ java.js
@@ -60,5 +60,69 @@ function vidersend() {
     $("#knap_container").addClass("vidersend");
     $("#finger_container").addClass("finger_move_up");
 
+    setTimeout(twoHandsIn, 5000);
+}
+
+function twoHandsIn() {
+    console.log("to hænder ind");
+
+    $("#mobil_container").removeClass("mobil_c");
+    $("#mobil_container").removeClass("mobil_c");
+    $("#knap_container").removeClass("vidersend");
+    $("#finger_container").removeClass("finger_move_up");
+
+    $("#baggrund_one").addClass("baggrund");
+    $("#hand_p").addClass("hands_one");
+    $("#hand_b").addClass("hand_brun");
+    setTimeout(allHands, 5000);
+}
+
+
+
+function allHands() {
+    console.log("alle hænder kommer ind");
+
+    $("#baggrund_one").removeClass("baggrund");
+    $("#hand_p").removeClass("hands_one");
+    $("#hand_b").removeClass("hand_brun");
+
+    $("#baggrund_two").addClass("baggrund_2");
+    $("#forskellig_hand_one").addClass("forskellig_one");
+    $("#forskellig_hand_two").addClass("forskellig_two");
+    setTimeout(vidersendKnap, 5000);
+}
+
+function vidersendKnap() {
+    console.log("tryk på vidersend knap");
+
+    $("#baggrund_two").removeClass("baggrund_2");
+    $("#forskellig_hand_one").removeClass("forskellig_one");
+    $("#forskellig_hand_two").removeClass("forskellig_two");
+
+    $("#vidersend").addClass("v_knap");
+    $("#finger_klik").addClass("f_klik");
+    setTimeout(laptopIpadIn, 5000);
+
+}
+
+function laptopIpadIn() {
+    console.log("laptop kommer ind");
+
+    $("#vidersend").removeClass("v_knap");
+    $("#finger_klik").removeClass("f_klik");
+
+    $("#comp").addClass("desktop");
+    $("#ipad").addClass("ipad_in");
+    $("#ipad").addClass("ipad_in");
+    $("#ipad_s").addClass("ipad_in_sort");
+    setTimeout(billedeIn, 6000);
+}
+
+function billedeIn() {
+    console.log("lbilleder der hører til");
+    $("#laptop_billede").addClass("censo_lap");
+    $("#ipad_h_billede").addClass("censo_ipad");
+    $("#ipad_s_billede").addClass("censo_s_ipad");
+
 
 }
